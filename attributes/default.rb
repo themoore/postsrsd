5ad4e056e666d440cb06da381137e9273aad5475
Patrick Moore <moore267@marshall.edu>
2014-05-02 12:54:07 -0400

    fixed repo location

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@
 #
 
 default['postsrsd']['buildroot'] = "#{Chef::Config[:file_cache_path]}/postsrsd"
-default['postsrsd']['repository'] = 'git@mugit03.marshall.edu:systems/mu-postsrsd.git'
+default['postsrsd']['repository'] = 'git://github.com/themoore/postsrsd'
 default['postsrsd']['packages'] = %w(cmake)
 
 default['postfix']['main']['sender_canonical_maps'] = 'tcp:127.0.0.1:10001'
