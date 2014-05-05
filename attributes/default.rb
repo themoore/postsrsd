ba106a6c214dfe3f836466811cfd1f1533c09e1b
Patrick Moore <moore267@marshall.edu>
2014-05-05 14:57:51 -0400

    changed git repo back to roehling

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@
 #
 
 default['postsrsd']['buildroot'] = "#{Chef::Config[:file_cache_path]}/postsrsd"
-default['postsrsd']['repository'] = 'git://github.com/themoore/postsrsd'
+default['postsrsd']['repository'] = 'git://github.com/roehling/postsrsd'
 default['postsrsd']['packages'] = %w(cmake)
 
 default['postfix']['main']['sender_canonical_maps'] = 'tcp:127.0.0.1:10001'
